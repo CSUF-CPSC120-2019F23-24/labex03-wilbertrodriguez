ec3204a83891da3d7c6efe841b53b50959be0ae4
wilbertrodriguez <wilbertrodriguez@csu.fullerton.edu>
2019-09-07 14:12:02 -0700

    Calculated height

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,17 @@
 // This program calculates a person's height in feet (') and inches (").
+
+#include <iostream>
+
+int main()
+{
+  double height;
+  std::cout << "What is your height? ";
+  std::cin >> height;
+  double heightinfeet1 = height / 12;
+  int heightinfeet2=heightinfeet1;
+  double heightininches1=heightinfeet1-heightinfeet2;
+  int heightininches2=heightininches1*12;
+  std::cout << heightinfeet2 <<"'"<< heightininches2<<"\"\n";
+
+  return 0;
+}
