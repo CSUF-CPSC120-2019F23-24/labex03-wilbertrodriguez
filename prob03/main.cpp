cdbe15f83bcb990155d2bf5a722354d613a63799
wilbertrodriguez <wilbertrodriguez@cs104u-6f.acad.fullerton.edu>
2019-09-14 15:04:54 -0700

    fixed the module

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -4,19 +4,15 @@
 
 int main()
 {
-  double height;
+  int height;
   std::cout << "What is your height? ";
   std::cin >> height;
   //obtains the height of the user
-  double heightinfeet1 = height / 12;
-  //Divides the height by inches in a foot
-  int heightinfeet2=heightinfeet1;
-  //removes the decimal
-  double heightininches1=heightinfeet1-heightinfeet2;
-  //obtains the decimal by removing the feet from the feet with the decimal
-  int heightininches2=heightininches1*12;
-  //Calculates the inches
-  std::cout << heightinfeet2 <<"'"<< heightininches2<<"\"\n";
+  int heightinfeet = height / 12;
+  //finds the height
+  double heightininches = height % 12;
+  //finds the inches
+  std::cout << heightinfeet <<"'"<< heightininches<<"\"\n";
   //sends out your height in feet and inches
 
   return 0;
