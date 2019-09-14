77e3ac425cbc85a481f631f95f4bd28ab13a6856
wilbertrodriguez <wilbertrodriguez@cs104u-6f.acad.fullerton.edu>
2019-09-14 14:56:59 -0700

    fixed problems with spacing

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -6,13 +6,13 @@ int main()
 {
   // one gallon of paint covers 340 square feet
   double coverage = 340;
-   double height = 6;
-    double length = 100;
+  double height = 6;
+  double length = 100;
 
   // calculate how many gallons of paint it will take to cover
   // the area with 2 coats of paint
-  double area=height*length*2;
-    double gallons = area / coverage;
+  double area = height * length * 2;
+  double gallons = area / coverage;
 
   std::cout << "It will take " << gallons << " gallons of paint.\n";
 
